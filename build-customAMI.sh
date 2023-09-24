d88d04b44a9371f7746f2ddd8aae515ad2b8449f
unknown <chetankakde2211gmail.com>
2023-09-24 15:03:52 +0530

    moving forward3

---
+++ build-customAMI.sh
@@ -0,0 +1,10 @@
+sudo  su
+cd
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install libtomcat9-java -y
+sudo apt-get update -y
+sudo apt-get install tomcat9-admin tomcat9-common -y
+sudo apt-get install tomcat9 -y
+cd /var/lib/tomcat9/webapps/
+sudo wget https://bucket-create-2211.s3.us-east-2.amazonaws.com/ion.war
+sudo systemctl start tomcat9 
